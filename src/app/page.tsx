c4991584c1b4c0ebb09d5c67b0e6e359733a69a0
kraken <solodarken@gmail.com>
2024-05-03 00:22:38 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,28 @@
 "use client";
+import { AnimatedBeam } from "@/components/ui/animated-beam";
 import { Button } from "@/components/ui/button";
+import GridPattern from "@/components/ui/grid-pattern";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
   return (
-    <div className=" flex flex-grow items-center flex-col">
-      <div className="absolute -z-10 bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
-      <div className="flex w-1/2 flex-grow gap-8 flex-col justify-center items-center h-full">
+    <div className=" flex flex-grow items-center w-full">
+      <GridPattern
+        width={30}
+        height={30}
+        x={-1}
+        y={-1}
+        className="[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
+      />
+      <div className="flex w-full lex-grow gap-8 flex-col justify-center items-center h-full">
         <div className="rounded-full border-[1px] border-primary w-fit px-4 py-2 backdrop-blur bg-secondary/50 ">
           <p>Personalised chatbot in minutes</p>
         </div>
         <h1 className="text-5xl font-bold text-primary tracking-wider">
           Invouge-Chat
         </h1>
-        <h1 className="text-3xl text-muted-foreground text-center whitespace-break-spaces">
+        <h1 className="text-3xl w-1/2 text-muted-foreground text-center whitespace-break-spaces">
           Revolutionize Conversations with AI-Driven Chatbot Mastery: Precision
           through Data Training and Dynamic Features for Unparalleled
           Interaction.
